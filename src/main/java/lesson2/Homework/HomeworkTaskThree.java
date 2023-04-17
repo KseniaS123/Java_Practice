14b3991549909ccd1354990303776c1ee1da1244
Ксения <you@example.com>
2023-04-17 16:42:47 +0300

    homework lesson 2 task 3 false

--- src/main/java/lesson2/Homework/HomeworkTaskThree.java
+++ src/main/java/lesson2/Homework/HomeworkTaskThree.java
@@ -5,33 +5,33 @@
 //        3 Расширение файла:
 //        4 Расширение файла: jpg
 
-package lesson2.Homework;
-
-import java.io.File;
-import java.util.Arrays;
-
-public class HomeworkTaskThree {
-    public static void main(String[] args) {
-
-        fileExtensionFromDirectory("D:\\Гикбрейнс");
-    }
-
-    private static String fileExtensionFromDirectory(String pathDir) {
-        File file = new File(pathDir);
-        if (!file.isDirectory()) {
-            return pathDir;
-        }
-
-        String[] dirListNames = file.list();
-        System.out.println(Arrays.toString(dirListNames));
-
-        StringBuilder sb = new StringBuilder();
-        for (String fileName : dirListNames) {
-            sb.append(fileName).append("\n");
-        }
-        System.out.println(sb);
-
-//            int index = sb.indexOf(".");
-        return pathDir;
-    }
-}
+//package lesson2.Homework;
+//
+//import java.io.File;
+//import java.util.Arrays;
+//
+//public class HomeworkTaskThree {
+//    public static void main(String[] args) {
+//
+//        fileExtensionFromDirectory("D:\\Гикбрейнс");
+//    }
+//
+//    private static String fileExtensionFromDirectory(String pathDir) {
+//        File file = new File(pathDir);
+//        if (!file.isDirectory()) {
+//            return pathDir;
+//        }
+//
+//        String[] dirListNames = file.list();
+//        System.out.println(Arrays.toString(dirListNames));
+//
+//        StringBuilder sb = new StringBuilder();
+//        for (String fileName : dirListNames) {
+//            sb.append(fileName).append("\n");
+//        }
+//        System.out.println(sb);
+//
+////            int index = sb.indexOf(".");
+//        return pathDir;
+//    }
+//}
