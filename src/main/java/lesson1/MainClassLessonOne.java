20d704330765042205095159cec7e6cf1321d25d
Ксения <you@example.com>
2023-04-17 13:07:03 +0300

    practice tasks lesson 1 task 1

--- src/main/java/lesson1/MainClassLessonOne.java
+++ src/main/java/lesson1/MainClassLessonOne.java
@@ -1,7 +1,16 @@
 package lesson1;
 
+import java.util.Scanner;
+
 public class MainClassLessonOne {
     public static void main(String[] args) {
-        
+        exOne();
+    }
+
+    private static void exOne() {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.printf("Привет, %s", name);
+        scanner.close();
     }
 }
