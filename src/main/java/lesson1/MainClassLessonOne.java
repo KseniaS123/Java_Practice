7ab3bbd5ffd17edaaa247b277f37d534b0edcabc
Ксения <you@example.com>
2023-04-17 13:11:17 +0300

    practice tasks lesson 1 task 2

--- src/main/java/lesson1/MainClassLessonOne.java
+++ src/main/java/lesson1/MainClassLessonOne.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class MainClassLessonOne {
     public static void main(String[] args) {
         exOne();
+        size2();
+
     }
 
     private static void exOne() {
@@ -12,5 +14,23 @@ public class MainClassLessonOne {
         String name = scanner.nextLine();
         System.out.printf("Привет, %s", name);
         scanner.close();
+    }
+
+    private static void size2() {
+        int[] arr = new int[]{1, 1, 0, 1, 1, 1, 0, 1};
+        int k = 0;
+        int kmax = 0;
+        for (int num : arr) {
+
+            if (num == 1) {
+                k++;
+                if (k > kmax) {
+                    kmax = k;
+                }
+            } else {
+                k = 0;
+            }
+        }
+        System.out.printf("Большая последовательность %s", kmax);
     }
 }
